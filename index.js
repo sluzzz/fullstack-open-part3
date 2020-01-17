0d672c0ec87eddaa6d240651e3c61fd5d669d784
sluzzz <franpanpan@gmail.com>
2020-01-17 14:58:21 +0100

    add config for heroku deployement

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   morgan(function(tokens, req, res) {
@@ -105,7 +107,7 @@ app.post('/api/persons', (request, response) => {
   response.json(body);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
