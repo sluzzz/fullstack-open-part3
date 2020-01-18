b42a173c17f8aba28d057e7553e67aa7c57fdf1d
unknown <franpanpan@gmail.com>
2020-01-18 19:17:05 +0100

    add static build files

---
+++ build/precache-manifest.f249c3aea6cd011be43d67457b58c357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22eea734fded7eabf1847fc8ba1db1b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b17b67a3b18ec6dffb7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f395e5039aaf708bc10b",
+    "url": "/static/js/2.58fbe66f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.58fbe66f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b17b67a3b18ec6dffb7",
+    "url": "/static/js/main.5416030a.chunk.js"
+  },
+  {
+    "revision": "4230d87a2d3cbabbdf4d",
+    "url": "/static/js/runtime-main.e53b3d9b.js"
+  }
+]);
